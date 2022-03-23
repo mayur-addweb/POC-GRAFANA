a177c669feea08125af1294c9ef58b846197bef1
Ubuntu <ubuntu@ip-172-31-37-245.us-east-2.compute.internal>
2022-03-23 10:02:51 +0000

    blackbox

--- ansible/roles/node-exporter/tasks/job.sh
+++ ansible/roles/node-exporter/tasks/job.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#sed -i "s/projectname/$project_name/g" /etc/nginx/sites-available/"$project_name".conf
+remote_ip=$1
+sed -i "s/ip/$remote_ip/g" /home/ubuntu/ansible/roles/node-exporter/tasks/job_name
 
 sudo cat /home/ubuntu/ansible/roles/node-exporter/tasks/job_name >> /usr/local/bin/prometheus/prometheus.yml
